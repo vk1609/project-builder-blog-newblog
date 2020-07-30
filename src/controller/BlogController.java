e6902dc464d11f69a7fd5bf1531864ba086d5297
unknown <vamsi3353@gmail.com>
2020-07-30 08:20:53 +0530

    vamsi

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Blog;
+import model.User;
 
 
 
@@ -31,12 +35,17 @@ public class BlogController extends HttpServlet {
 	}
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response, Object user) throws ServletException, IOException {
+		String blogDetails = request.getParameter("selectedAnswers");
+		String str[] = blogDetails.split(",");
+		 String name=str[0];
+	     String description=str[1];
+	     LocalDate date= LocalDate.now();
 		
-		
-		
-
+	     Blog blog=new Blog(name,description,date);
+		 System.out.println(name);
+		 System.out.println(description);
+		 System.out.println(date);
 		
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
@@ -44,7 +53,7 @@ public class BlogController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+		
 	}
 
 }
--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,39 @@
 package model;
+
+import java.time.LocalDate;
+
 // Type your code
+public class Blog
+{
+	
+	//arguments
+	String title;
+	String description;
+	LocalDate postedOn;
+	
+	public Blog(String title, String description, LocalDate postedOn) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.postedOn = postedOn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getPostedOn() {
+		return postedOn;
+	}
+	public void setPostedOn(LocalDate postedOn) {
+		this.postedOn = postedOn;
+	}
+	
+}
